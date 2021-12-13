817fb8a44fc3782e8368b4cf7b6b29cfba00a29e
Corey <coreyg142@gmail.com>
2021-12-12 20:22:55 -0500

    initial commit

---
+++ src/main/java/io/github/coreyg142/emotemenu/EmoteMenu.java
@@ -0,0 +1,17 @@
+package io.github.coreyg142.emotemenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EmoteMenu extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
