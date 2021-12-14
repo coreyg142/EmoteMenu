d0c5af64c33e09eb3690c19cd6f5b6608c92c867
Corey <coreyg142@gmail.com>
2021-12-13 20:14:36 -0500

    Using config file instead of hardcoding in emotes.

--- src/main/java/io/github/coreyg142/emotemenu/EmoteMenu.java
+++ src/main/java/io/github/coreyg142/emotemenu/EmoteMenu.java
@@ -4,20 +4,32 @@ import io.github.coreyg142.emotemenu.commands.CommandEmotes;
 import io.github.coreyg142.emotemenu.util.Emote;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
 import java.util.Objects;
-
+import java.util.logging.Logger;
 
 public final class EmoteMenu extends JavaPlugin {
 
+	public static Logger logger;
 
 	@Override
 	public void onEnable() {
 		// Plugin startup logic
+		this.saveDefaultConfig();
+		logger = getLogger();
 
-		Emote.populateTwitchEmotes();
+		List<String> emotes = this.getConfig().getStringList("TwitchEmotes");
+		boolean success = Emote.populateTwitchEmotes(emotes);
+
+		if(!success) {
+			getLogger().severe("Failed to initialize plugin, is the config in the right format?");
+			return;
+		}
+
 		getLogger().info(String.format("Loaded %d emotes", Emote.TWITCH_EMOTES.size()));
 		Objects.requireNonNull(this.getCommand("emotes")).setExecutor(new CommandEmotes());
 		getLogger().info("Emote menu is initialized!");
+
 	}
 
 	@Override
